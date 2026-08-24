75ebf2d9cea2555aed8a7997eca8433b590a81ec
Alexis <alexismhw@yahoo.com>
2026-08-24 10:05:19 -0400

    Regenerate tina-lock.json after schema fix

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -50,7 +50,7 @@ var config_default = defineConfig({
             type: "string",
             name: "byline",
             label: "Byline",
-            ui: { defaultItemValue: "The Coordinated Host by Place & Plenty" }
+            ui: { defaultValue: "The Coordinated Host by Place & Plenty" }
           },
           { type: "datetime", name: "publishDate", label: "Publish date" },
           { type: "datetime", name: "updatedDate", label: "Updated date" },
